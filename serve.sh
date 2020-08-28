b8fb1a18e2fc6dc70195ee4d7a88047bde0c0369
Philip Fourie <philip.fourie@theta.co.nz>
2020-08-29 11:29:11 +1200

    Change to serve files from docker container
    This makes it easier to quickly start blogging on any docker
    supported environment.

--- serve.sh
+++ serve.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # slow but use the same build as used on Github Actions
-#exec docker run --rm --volume $PWD:/src --publish "0.0.0.0:1313:1313" bric3/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --buildDrafts
+exec docker run --rm --volume $PWD:/src -p "1313:1313" philipf/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --verbose --buildDrafts --buildFuture
 
-exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture
+# Requires local installation of Hugo and dependencies
+#exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture
