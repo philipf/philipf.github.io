1399490686c7bf83e40b04f68943f3e3e8293fb9
Philip Fourie <philip.fourie@theta.co.nz>
2020-09-06 16:14:57 +1200

    Don't render Google Analytics on localhost

--- serve.sh
+++ serve.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # slow but use the same build as used on Github Actions
-exec docker run --rm --volume $PWD:/src -p "1313:1313" philipf/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=blog.local --verbose --buildDrafts --buildFuture
+exec docker run --rm --volume $PWD:/src -p "1313:1313" philipf/hugo-builder hugo serve --bind=0.0.0.0 --baseUrl=localhost --verbose --buildDrafts --buildFuture
 
 # Requires local installation of Hugo and dependencies
 #exec env PATH=$PWD/bin:$PATH hugo serve --verbose --baseUrl=blog.local --bind=0.0.0.0 --buildDrafts --buildFuture
